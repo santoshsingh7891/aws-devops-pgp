f2fc5c161d8df0f845852f357ad3e0ca44169d01
santoshsingh7891 <singh.santosh7891@gmail.com>
2025-09-09 03:35:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 custombucket = "destinationtxt"
 table = "intellipaat"
-databasehost = "database-1.c14bnmt8jlz0.us-east-1.rds.amazonaws.com"
+databasehost = "database-1.cleokuouukfq.us-east-1.rds.amazonaws.com"
 duser = "intel"
 dpass = "intel123"
 s3database = "intellipaat"
